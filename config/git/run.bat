124158aff9a757ca55503cca2767715120cd284a
yzlc <yzlc233@outlook.com>
2023-01-06 15:20:21 +0800

    1

--- config/git/run.bat
+++ config/git/run.bat
@@ -1,3 +1,10 @@
 git config --global user.name "yzlc"
 git config --global user.email yzlc233@outlook.com
+
+git config --global pull.rebase false
+git config --global pull.ff false
+
+copy .gitconfig %USERPROFILE%
+git config --global core.excludesfile .gitignore
+
 exit

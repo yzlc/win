851e84bb249bc61394080a255e07bb539e7a117c
yzlc <yzlc233@outlook.com>
2023-02-09 19:59:22 +0800

    1

--- config/disableService/run.bat
+++ config/disableService/run.bat
@@ -1,3 +1,6 @@
 @cd/d"%~dp0"&(cacls "%SystemDrive%\System Volume Information" >nul 2>nul)||(start "" mshta vbscript:CreateObject^("Shell.Application"^).ShellExecute^("%~nx0"," %*","","runas",1^)^(window.close^)&exit /b)
+# 遥测
 sc config DiagTrack start= demand
+sc config SysMain start= demand
+sc config WSearch start= demand
 exit

1a2fc5b51b3f8141ddcd6a8c9ddfde98bd5beeda
lim <yzlc233@outlook.com>
2023-12-01 10:34:25 +0800

    1

--- config/git/run.bat
+++ config/git/run.bat
@@ -1,10 +1,12 @@
-git config --global user.name "yzlc"
+git config --global user.name "lim"
 git config --global user.email yzlc233@outlook.com
 
 git config --global pull.rebase false
 git config --global pull.ff false
 
+git config --global https.sslVerify false
+
 git config --global core.excludesfile .gitignore
-git config --global credential.helper manager
+git config --global credential.helper store
 
 exit

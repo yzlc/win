1d7020b70b0e2c25ba09ddffec7a56908589c34b
lim <yzlc233@outlook.com>
2023-12-24 11:22:58 +0800

    1

--- config/git/run.bat
+++ config/git/run.bat
@@ -9,4 +9,7 @@ git config --global https.sslVerify false
 git config --global core.excludesfile .gitignore
 git config --global credential.helper store
 
+git config --global http.https://github.com.proxy http://127.0.0.1:10809
+git config --global https.https://github.com.proxy https://127.0.0.1:10809
+
 exit

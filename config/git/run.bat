f3afbcb73b2db185f92709b803575916112a0fc6
lim <yzlc233@outlook.com>
2024-04-15 09:33:42 +0800

    1

--- config/git/run.bat
+++ config/git/run.bat
@@ -2,7 +2,6 @@ git config --global user.name "lim"
 git config --global user.email yzlc233@outlook.com
 
 git config --global pull.rebase false
-git config --global pull.ff false
 
 git config --global https.sslVerify false
 

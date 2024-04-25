c7a9db0df6b7d303fb8c260283b289b6d97166e1
lim <yzlc233@outlook.com>
2024-04-25 09:17:40 +0800

    1

--- config/git/run.bat
+++ config/git/run.bat
@@ -1,3 +1,5 @@
+copy .gitignore %USERPROFILE%
+
 git config --global user.name "lim"
 git config --global user.email yzlc233@outlook.com
 
@@ -5,7 +7,7 @@ git config --global pull.rebase false
 
 git config --global https.sslVerify false
 
-git config --global core.excludesfile .gitignore
+git config --global core.excludesfile %USERPROFILE%/.gitignore
 git config --global credential.helper store
 
 git config --global http.https://github.com.proxy http://127.0.0.1:10809
